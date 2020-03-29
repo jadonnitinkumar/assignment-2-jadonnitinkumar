f7d4a876432dbe57772631b0898b0f54e664997d
Nitin Kumar <nitin.kumar_cs18@gla.ac.in>
2020-03-29 13:46:00 +0530

    create this method to get root

---
+++ src/problem2/main/MyTraverse.java
@@ -0,0 +1,14 @@
+package problem2.main;
+
+import problem1.node.TreeNode;
+
+public class MyTraverse {
+    TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void insert(int data) {
+    }
+}
