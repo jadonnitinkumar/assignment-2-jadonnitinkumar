2ee26272085acf074c68b32398f3452ff48da741
Nitin Kumar <nitin.kumar_cs18@gla.ac.in>
2020-03-29 14:15:05 +0530

    create Treenode getRoot()

--- src/problem2/main/MyTraverse.java
+++ src/problem2/main/MyTraverse.java
@@ -6,6 +6,7 @@ public class MyTraverse {
     public TreeNode getRoot() {
         return root;
     }
+
     public void insert(int data) {
         TreeNode node = new TreeNode(data);
         if (root == null) {
